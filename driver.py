e1d1808483156c92b1da0a2024bbb3ebb7e61674
Luke Roepe <ldroepe@gmail.com>
2023-04-15 17:29:12 -0400

    Add Sorting Algorithm Selection
    Add command line arguments for each of the sorting algorithms that are
    currently implemented

--- driver.py
+++ driver.py
@@ -1,8 +1,24 @@
-
+import argparse
 from sorting import *
+
+SORTING_ALGORITHMS = ['bubble', 'insertion', 'selection', 'cocktail']
 
 if __name__ == '__main__':
     
-    sorts = ['bubble', 'insertion', 'selection', 'cocktail']
-    for sort_name in sorts:
-        globals()[sort_name+'_sort']()
+    parser = argparse.ArgumentParser(description='Sorting algorithm implementations')
+
+    sorting_algo_group = parser.add_mutually_exclusive_group(required=True)
+    for sorting_algo in SORTING_ALGORITHMS:
+
+        abbrev = f'-{sorting_algo[0]}'
+        full_option = f'--{sorting_algo}-sort'
+        sorting_algo_group.add_argument(abbrev, full_option, action='store_true')
+
+    args = parser.parse_args()
+
+    for algo, chosen in args._get_kwargs():
+        if chosen:
+            
+            globals()[algo]()
+            exit(0)
+
