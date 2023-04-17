b9fe7731de83a4f597eb91397f3b115ee0832fbd
Luke Roepe <ldroepe@gmail.com>
2023-04-16 20:47:04 -0400

    Add basic GUI
    Four buttons (to select which sorting algorithm you would like to
    perform), and two labels to show the list of numbers you are sorting
    before and after having sorted them.
    
    Currently all of the command line options are just commented out. This
    will change in future versions of the code.

--- driver.py
+++ driver.py
@@ -1,14 +1,59 @@
 import random
+import sorting
 import argparse
-from sorting import *
+import tkinter as tk
 
 SORTING_ALGORITHMS = ['bubble', 'insertion', 'selection', 'cocktail']
+
+class SortingApplication(tk.Frame):
+    
+    def __init__(self, n, max_value, master=None):
+
+        super().__init__(master)
+
+        self.n = n
+        self.max_value = max_value
+        self.master = master
+
+        self.original_list_label = tk.Label(self, text='[]')
+        self.original_list_label.pack(side='top')
+
+        self.sorted_list_label = tk.Label(self, text='')
+        self.sorted_list_label.pack(side='top')
+
+        self.create_buttons()
+        self.pack()
+
+    def create_buttons(self):
+        
+        sorting_algorithm_fns = vars(sorting)
+
+        def create_button_action(full_sorting_algo_name):
+            def button_action():
+                nums = [random.randint(0, self.max_value) for _ in range(self.n)]
+                self.original_list_label['text'] = nums
+                sorting_algorithm_fns[full_sorting_algo_name](nums)
+                self.sorted_list_label['text'] = nums
+
+            return button_action
+
+        for algo_name in SORTING_ALGORITHMS:
+            
+            sorting_button = tk.Button(self)
+            full_sorting_algo_name = f'{algo_name}_sort'
+
+            sorting_button['text'] = full_sorting_algo_name
+            sorting_button['command'] = create_button_action(full_sorting_algo_name)
+
+            sorting_button.pack(side='left')
+            self.__dict__[f'{algo_name}_button'] = sorting_button
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Sorting algorithm implementations')
 
-    sorting_algo_group = parser.add_mutually_exclusive_group(required=True)
+    #sorting_algo_group = parser.add_mutually_exclusive_group(required=True)
+    sorting_algo_group = parser.add_mutually_exclusive_group()
     for sorting_algo in SORTING_ALGORITHMS:
 
         abbrev = f'-{sorting_algo[0]}'
@@ -24,9 +69,14 @@ if __name__ == '__main__':
     parser.add_argument('--max-value', type=int,
                         help='maximum value that can appear in the list to sort',
                         default=100)
-
     args = parser.parse_args()
 
+    root = tk.Tk()
+    app = SortingApplication(args.n, args.max_value, master=root)
+    app.master.title('Sorting')
+    app.mainloop()
+
+    '''
     sort_options = [(arg, val) for arg, val in args._get_kwargs() if 'sort' in arg]
     nums = [random.randint(0, args.max_value) for i in range(args.n)]
 
@@ -35,3 +85,4 @@ if __name__ == '__main__':
             
             globals()[algo](nums)
             exit(0)
+    '''
